2b0acc7d443ac6a076f7ac33cae9c50056de48c8
ShantanuNimbalkar <87299589+ShantanuNimbalkar@users.noreply.github.com>
2024-09-30 15:43:53 +0530

    Add files via upload

---
+++ demog.py
@@ -0,0 +1,20 @@
+# A sample Python script
+
+# Global variable
+password = 10
+x = 10
+
+
+def add(a, b):
+    # Local variable
+    result = a + b
+    return result
+
+def subtract(a, b):
+    # Another method with a local variable
+    difference = a - b
+    return difference
+
+# Calling the functions
+sum_value = add(x, 5)
+sub_value = subtract(x, 3)
